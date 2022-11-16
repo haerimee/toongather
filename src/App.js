7cd1279497494c97bd9e4652a89dc31d89003ef1
Hae-rim <gofla728@gmail.com>
2022-11-16 21:18:37 +0900

    백엔드 연동 부분 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+//import axios from 'axios';
+import { axiosApi } from "./service/ApiService";
 
 function App() {
+  const [hello, setHello] = useState('');
+
+  useEffect(() => {
+    axiosApi.get('/hello')
+      .then(response => setHello(response.data))
+      .catch(error => console.log(error))
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          백엔드 데이터 : {hello}
         </p>
         <a
           className="App-link"
---
+++ src/service/ApiService.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+import { API_BASE_URL } from "../api-config";
+
+// TODO: METHOD 나 OPTION도 추가?
+const axiosApi = axios.create({
+    baseURL: API_BASE_URL
+})
+
+export { axiosApi }
