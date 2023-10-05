3b1dfb0a156c2a41d1070f375104d481830d70a8
haerimee <gofla728@gmail.com>
2023-10-05 21:42:21 +0900

    feat 로그인 화면 초기 작업 진행

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,47 +1,54 @@
-import { Helmet } from 'react-helmet-async';
+import { Helmet } from "react-helmet-async";
 // @mui
-import { styled } from '@mui/material/styles';
-import { Link, Container, Typography, Divider, Stack, Button } from '@mui/material';
+import { styled } from "@mui/material/styles";
+import {
+  Link,
+  Container,
+  Typography,
+  Divider,
+  Stack,
+  Button,
+} from "@mui/material";
 // hooks
-import useResponsive from '../hooks/useResponsive';
+import useResponsive from "../hooks/useResponsive";
 // components
-import Logo from '../components/logo';
-import Iconify from '../components/iconify';
+import Logo from "../components/logo";
+import Iconify from "../components/iconify";
 // sections
-import { LoginForm } from '../sections/auth/login';
+import { LoginForm } from "../sections/auth/login";
 
 // ----------------------------------------------------------------------
 
-const StyledRoot = styled('div')(({ theme }) => ({
-  [theme.breakpoints.up('md')]: {
-    display: 'flex',
+const StyledRoot = styled("div")(({ theme }) => ({
+  [theme.breakpoints.up("md")]: {
+    display: "flex",
   },
 }));
 
-const StyledSection = styled('div')(({ theme }) => ({
-  width: '100%',
+const StyledSection = styled("div")(({ theme }) => ({
+  width: "100%",
   maxWidth: 480,
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
   boxShadow: theme.customShadows.card,
   backgroundColor: theme.palette.background.default,
 }));
 
-const StyledContent = styled('div')(({ theme }) => ({
+const StyledContent = styled("div")(({ theme }) => ({
   maxWidth: 480,
-  margin: 'auto',
-  minHeight: '100vh',
-  display: 'flex',
-  justifyContent: 'center',
-  flexDirection: 'column',
+  margin: "auto",
+  minHeight: "100vh",
+  display: "flex",
+  justifyContent: "center",
+  flexDirection: "column",
   padding: theme.spacing(12, 0),
 }));
 
 // ----------------------------------------------------------------------
 
 export default function LoginPage() {
-  const mdUp = useResponsive('up', 'md');
+  const mdUp = useResponsive("up", "md");
 
   return (
     <>
@@ -50,7 +57,7 @@ export default function LoginPage() {
       </Helmet>
 
       <StyledRoot>
-        <Logo
+        {/* <Logo
           sx={{
             position: 'fixed',
             top: { xs: 16, sm: 24, md: 40 },
@@ -65,35 +72,50 @@ export default function LoginPage() {
             </Typography>
             <img src="/assets/illustrations/illustration_login.png" alt="login" />
           </StyledSection>
-        )}
+        )} */}
 
         <Container maxWidth="sm">
           <StyledContent>
             <Typography variant="h4" gutterBottom>
-              Sign in to Minimal
+              ToonGather
             </Typography>
 
             <Typography variant="body2" sx={{ mb: 5 }}>
-              Don’t have an account? {''}
+              Don’t have an account? {""}
               <Link variant="subtitle2">Get started</Link>
             </Typography>
 
             <Stack direction="row" spacing={2}>
               <Button fullWidth size="large" color="inherit" variant="outlined">
-                <Iconify icon="eva:google-fill" color="#DF3E30" width={22} height={22} />
+                <Iconify
+                  icon="simple-icons:kakaotalk"
+                  color="#fee500"
+                  width={22}
+                  height={22}
+                />
               </Button>
 
               <Button fullWidth size="large" color="inherit" variant="outlined">
-                <Iconify icon="eva:facebook-fill" color="#1877F2" width={22} height={22} />
+                <Iconify
+                  icon="flat-color-icons:google"
+                  color="#2db400"
+                  width={22}
+                  height={22}
+                />
               </Button>
 
               <Button fullWidth size="large" color="inherit" variant="outlined">
-                <Iconify icon="eva:twitter-fill" color="#1C9CEA" width={22} height={22} />
+                <Iconify
+                  icon="simple-icons:naver"
+                  color="#2db400"
+                  width={20}
+                  height={20}
+                />
               </Button>
             </Stack>
 
             <Divider sx={{ my: 3 }}>
-              <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+              <Typography variant="body2" sx={{ color: "text.secondary" }}>
                 OR
               </Typography>
             </Divider>
